79c9c4779804fad86d4308cf1a9fd96fbc89ef21
Daniel Ribeiro dos Santos <drsantos20@gmail.com>
2019-04-17 08:08:40 -0300

    Add VueResource to the API integration

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import VueResource from 'vue-resource';
+
+//Register VueResource inside the global view object of Vue
+Vue.use(VueResource)
+
 new Vue({
   el: '#app',
   render: h => h(App)
